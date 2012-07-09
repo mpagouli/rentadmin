d213c3e04f9459fb5c5bac77f113c549c15f9518
Nina <mpagouli@gmail.com>
2012-07-09 15:07:00 +0300

    Home and Help static pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rentadmin::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,10 +50,12 @@ Rentadmin::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match '/help', to: 'static_pages#help'
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  subject { page }
+
+  shared_examples_for "all static pages" do
+  	it "having page specific h1" do  
+  		should have_selector('h1', :text => page_title) 
+  	end
+    it 'having page specific title' do
+    	should have_selector('title', :text => page_title)
+	end
+  end
+
+  describe "Home Page" do
+  	before { visit root_path } 
+  	let(:page_title) { 'Home Page' }
+  	it_should_behave_like "all static pages"
+  end
+
+  describe "Help Page" do
+  	before { visit help_path } 
+  	let(:page_title) { 'Help' }
+  	it_should_behave_like "all static pages"
+  end
+
+
+end
